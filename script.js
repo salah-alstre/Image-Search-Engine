635f9a1fde8e8805cc7d5bf5d93e1315a60f0f79
Salah-Dev <134534902+salah-alstre@users.noreply.github.com>
2024-10-18 01:23:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let page = 1;
 
 async function searchImages() {
     keyword = searchBox.value; 
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${acceskey}&per_page=12`; // تأكد من تعريف url بشكل صحيح
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${acceskey}&per_page=12`;
 
     try {
         const response = await fetch(url); 

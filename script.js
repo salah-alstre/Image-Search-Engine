2d74f6a8f115b9015deda19622a3eae3fe9d4161
salah <salah1999fbi0@gmail.com>
2024-10-18 01:20:46 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const acceskey = "kxBJ7aAHdJqcKy8mFLa-eitva8uwzzuUAxEWiyh93xc";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+    keyword = searchBox.value; 
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${acceskey}&per_page=12`; // تأكد من تعريف url بشكل صحيح
+
+    try {
+        const response = await fetch(url); 
+        const data = await response.json();
+        const results = data.results;
+
+        if(page ===1){
+            searchResult.innerHTML = "";
+        }
+
+        if (results) {
+            results.forEach((result) => {
+                const image = document.createElement("img");
+                image.src = result.urls.small;
+                const imageLink = document.createElement("a");
+                imageLink.href = result.links.html;
+                imageLink.target = "_blank";
+
+                imageLink.appendChild(image);
+                searchResult.appendChild(imageLink);
+            });
+        }
+    } catch (error) {
+        console.error('Error fetching images', error);
+    }
+    showMoreBtn.style.display = "block";
+    
+}
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click",()=>{
+    page++;
+    searchImages();
+})
